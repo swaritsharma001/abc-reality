3ed54eb61473df34f05581f7fe25af2c9a4c3fbe
UJJWAL SHARMA <152766614+swaritsharma001@users.noreply.github.com>
2025-08-22 18:00:44 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    allowedHosts: ["3d823910-1fbf-4fac-a626-4330d4445ce9-00-29jtzdjuaifx7.pike.replit.dev"]
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
